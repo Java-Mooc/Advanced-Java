0ed201409a456c09723b1fc88b0c617b1f2a1fa6
Rossana <150118036@esg.ipsantarem.pt>
2018-04-16 18:39:55 +0100

    First Commit

---
+++ Code/Semana 1/src/ppt1/Main.java
@@ -0,0 +1,35 @@
+package ppt1;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			//A tentar conectarmos com o url
+			URL url = new URL("http://www.columbia.edu/~fdc/sample.html");
+			String textoRecebido = "";
+			
+			//Aqui pedimos a página web em questão
+			URLConnection conexaoAoWebsite = url.openConnection();
+			
+			//Stream onde recebemos os dados
+			InputStream streamDeLeitura = url.openStream();
+			//Buffer onde armazenamos os dados
+	        BufferedReader buffer = new BufferedReader(new InputStreamReader(streamDeLeitura));
+	        
+	        String textoDoBuffer;
+	        while ((textoDoBuffer = buffer.readLine()) != null) {
+	            textoRecebido += textoDoBuffer;
+	        }
+	        
+			System.out.println(textoRecebido);
+		} catch (Exception e){
+			System.err.println("Ocorreu uma excepção!\n" + e.getMessage());
+		} 
+	}
+}
